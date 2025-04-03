2bb1934128e8c0792bc42c2fe70b084e5501e689
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-03 16:18:49 +0200

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
+import Linklist from "./LinkList"
+
 export default function Header() {
-    return <header></header>
+    return <header className="container">
+
+        <div>
+            <img src="dc-logo.png" alt="DC Logo" />
+        </div>
+        <Linklist />
+
+    </header>
 }
---
+++ src/components/LinkList.jsx
@@ -0,0 +1,14 @@
+export default function Linklist() {
+    return <ul>
+        <a href="#"><li>CHARACTERS</li></a>
+        <a href="#"><li>COMICS</li></a>
+        <a href="#"><li>MOVIES</li></a>
+        <a href="#"><li>TV</li></a>
+        <a href="#"><li>GAMES</li></a>
+        <a href="#"><li>COLLECTIBLES</li></a>
+        <a href="#"><li>VIDEOS</li></a>
+        <a href="#"><li>FANS</li></a>
+        <a href="#"><li>NEWS</li></a>
+        <a href="#"><li>SHOP</li></a>
+    </ul>
+}
