a2c85a371b3d7b7424a761c9306f591a4344e399
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-07 11:47:25 +0200

    feat: header links list iter.

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,14 +1,5 @@
 export default function Linklist({ headerLinks }) {
     return <ul>
-        <a href="#"><li>{headerLinks[0]}</li></a>
-        <a href="#"><li>{headerLinks[1]}</li></a>
-        <a href="#"><li>{headerLinks[2]}</li></a>
-        <a href="#"><li>{headerLinks[3]}</li></a>
-        <a href="#"><li>{headerLinks[4]}</li></a>
-        <a href="#"><li>{headerLinks[5]}</li></a>
-        <a href="#"><li>{headerLinks[6]}</li></a>
-        <a href="#"><li>{headerLinks[7]}</li></a>
-        <a href="#"><li>{headerLinks[8]}</li></a>
-        <a href="#"><li>{headerLinks[9]}</li></a>
+        {headerLinks.map(link => <a href="#"><li>{link}</li></a>)}
     </ul>
 }
