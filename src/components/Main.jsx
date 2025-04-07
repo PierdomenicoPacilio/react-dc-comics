fbbd16e093fe45c7c84ff4e9f09c18e839bdefe8
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-07 19:36:42 +0200

    feat: jumbotron

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,8 @@
+import jumbotron from './../assets/img/jumbotron.jpg'
+
 export default function Main() {
     return <main>
+        <div id='jumbotron'><img src={jumbotron} alt="jumbotron" /></div>
         <div className="container"><div>{"-->"} content goes here {"<--"}</div></div>
     </main>
 }
